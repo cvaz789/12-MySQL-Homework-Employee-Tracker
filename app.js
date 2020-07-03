86bf2136d0e8a5f394773db7ccd834819c75fbd3
Rodia Raskolnikof <cvaz789@gmail.com>
2020-07-02 23:59:38 -0500

    Start script not working for heroku

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extendend:true}));
+app.use(express.json());
+
+ 
+
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
